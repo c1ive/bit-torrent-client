3314423752acfa89d1ad839eb3c184d1605231c1
Jonas Sasowski <sasowski@outlook.de>
2025-11-16 20:43:36 +0100

    Tests: Bencode: add more integer tests

--- tests/BencodeParserTests.cpp
+++ tests/BencodeParserTests.cpp
@@ -1,5 +1,6 @@
 #define DOCTEST_CONFIG_IMPLEMENT_WITH_MAIN
 #include <doctest.h>
+#include <limits>
 
 #include "BencodeParser.hpp"
 
@@ -29,6 +30,18 @@ TEST_CASE("Parse integer 4") {
     REQUIRE(std::get<int64_t>(value) == 12093810981093);
 }
 
+TEST_CASE("Parse integer int64 max") {
+    auto value = bencode::parse("i9223372036854775807e");
+    REQUIRE(std::holds_alternative<int64_t>(value));
+    REQUIRE(std::get<int64_t>(value) == std::numeric_limits<int64_t>::max());
+}
+
+TEST_CASE("Parse integer int64 min") {
+    auto value = bencode::parse("i-9223372036854775808e");
+    REQUIRE(std::holds_alternative<int64_t>(value));
+    REQUIRE(std::get<int64_t>(value) == std::numeric_limits<int64_t>::min());
+}
+
 TEST_CASE("Parse integer negative zero") {
     REQUIRE_THROWS_AS(bencode::parse("i-0e"), std::invalid_argument);
 }
@@ -49,8 +62,32 @@ TEST_CASE("Parse integer overflow") {
     REQUIRE_THROWS_AS(bencode::parse("i9223372036854775808e"), std::out_of_range);
 }
 
+TEST_CASE("Parse integer underflow") {
+    REQUIRE_THROWS_AS(bencode::parse("i-9223372036854775809e"), std::out_of_range);
+}
+
 TEST_CASE("Parse invalid integer") {
     REQUIRE_THROWS_AS(bencode::parse("i12a3e"), std::invalid_argument);
+}
+
+TEST_CASE("Parse integer with plus sign") {
+    REQUIRE_THROWS_AS(bencode::parse("i+3e"), std::invalid_argument);
+}
+
+TEST_CASE("Parse integer with whitespace") {
+    REQUIRE_THROWS_AS(bencode::parse("i 3e"), std::invalid_argument);
+}
+
+TEST_CASE("Parse integer negative leading zero") {
+    REQUIRE_THROWS_AS(bencode::parse("i-0123e"), std::invalid_argument);
+}
+
+TEST_CASE("Parse integer double minus") {
+    REQUIRE_THROWS_AS(bencode::parse("i--1e"), std::invalid_argument);
+}
+
+TEST_CASE("Parse integer missing digits after minus") {
+    REQUIRE_THROWS_AS(bencode::parse("i-e"), std::invalid_argument);
 }
 
 TEST_CASE("Parse string") {
